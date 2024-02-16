dc5e1e5c5a4f3fa92864575ae1f6732276c2da67
Susil Kumar Behera <susil.behera@utkallabs.com>
2024-02-16 12:47:47 +0530

    fresh project

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.3.0',
+  'full_app_version' => 'v6.3.0 - build 12490-g9136415bb',
+  'build_version' => '12490',
+  'prerelease_version' => '',
+  'hash_version' => 'g9136415bb',
+  'full_hash' => 'v6.3.0-729-g9136415bb',
+  'branch' => 'master',
+);
---
+++ resources/lang/sr-CS/admin/companies/table.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'companies' => 'Firme',
+    'create'    => 'Kreiraj firmu',
+    'email'     => 'Company Email',
+    'title'     => 'Firma',
+    'phone'     => 'Company Phone',
+    'update'    => 'Ažuriraj firmu',
+    'name'      => 'Nayiv firme',
+    'id'        => 'ID',
+);
